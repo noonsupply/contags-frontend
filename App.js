abe82d3eb2d31c7aac8561e3f5489d9efef3f00e
gabin <gabin.subri@gmail.com>
2022-12-12 14:08:33 +0100

    Finish front email

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image style={styles.logo} source={require("./assets/logo.png")}></Image>
+      <View style={styles.inputText}>
+        <Text style={styles.text}>Saisissez votre adresse mail</Text>
+        <TextInput style={styles.input} placeholder={"Email"}></TextInput>
+      </View>
+      <View style={styles.caseButton}>
+        <TouchableOpacity style={styles.button}>
+          <FontAwesome color="#0031B8" name="chevron-left" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button2}>
+          <FontAwesome color="#0031B8" name="chevron-right" />
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,55 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#0031B8",
+    display: "flex",
+  },
+  logo: {
+    width: 293,
+    height: 102,
+    left: 50,
+    top: 96,
+  },
+  inputText: {
+    marginTop: 250,
+    marginLeft: 25,
+    marginRight: 25,
+  },
+  text: {
+    color: "#ffffff",
+    marginBottom: 10,
+  },
+  input: {
+    height: 60,
+    borderRadius: 5,
+    backgroundColor: "#ffffff",
+    paddingLeft: 15,
+  },
+  icon: {
+    color: "#ffffff",
+  },
+  caseButton: {
+    flexWrap: "wrap",
+    flexDirection: "row",
+    marginTop: 320,
+    justifyContent: "space-between",
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  button: {
+    backgroundColor: "#ffffff",
+    height: 50,
+    width: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+  },
+  button2: {
+    backgroundColor: "#ffffff",
+    height: 50,
+    width: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
   },
 });
