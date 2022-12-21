142279f801595f97b4ce99498074633c117d9952
Matthieu Vinel01~ <matthieu.vinel@gmail.com[201~>
2022-12-21 22:45:58 +0100

    forgot to change to MainNavigator in App.js

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <SafeAreaProvider>
-        <TagCreation />
+        <MainNavigator />
       </SafeAreaProvider>
     </Provider>
   );
