61c92644a10219367a9a931154abba3cfc26f016
GROMETTO N <nicolas.grometto@laposte.net>
2022-12-21 14:26:48 +0100

    before checkout

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -20,16 +20,16 @@ const MainNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash">
-        {/* <Stack.Screen
+         <Stack.Screen
           name="Splash"
           options={{ animationEnabled: false, header: () => null }}
           component={SplashScreen}
         />
-        <Stack.Screen
+        {/* <Stack.Screen
           name="SignInPonctuelScreen"
           options={{ animationEnabled: true, header: () => null }}
           component={SignInPonctuelScreen}
-        />
+        /> */}
         <Stack.Screen
           name="MailScreen"
           options={{ animationEnabled: true, header: () => null }}
@@ -54,7 +54,7 @@ const MainNavigator = () => {
           name="HomeLoadContact"
           options={{ animationEnabled: true, header: () => null }}
           component={HomeLoadContact}
-        /> */}
+        /> 
         <Stack.Screen
           name="HomeScreen"
           options={{ animationEnabled: true, header: () => null }}
